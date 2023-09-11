5cf33a9a527523de64b41f864e3be950e9c35326
Luis Enrique <luiskike2210@gmail.com>
2023-09-11 15:09:09 -0600

    03 Tipos de Variable y Tipos de datos

--- TiposyVariables.js
+++ TiposyVariables.js
@@ -15,3 +15,22 @@ console.log(typeof(12));
 
  console.log([1,2,3]);
  console.log(typeof([1,2,3]));
+
+ //variables en JavaScript
+ //versiones anteriores ES6 var
+ var firstName='Luis Enrique';
+
+ //const y let es de ES6 en adelante
+ //constantes const (variables de solo lectura)
+ //Siempre debe inicializarse
+ //No permite reasignacion
+
+ const PI = 3.1416;
+ // no se puede hacer esto PI=3.1416;
+
+ //variables con let para cambiar datos o que van cambiando 
+ //declarar
+ let lastName;
+ lastName='Sanchez'; 
+ lastName=true; 
+ lastName=25; 
