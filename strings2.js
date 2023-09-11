e60ff24fb18b2cc736560c91476fbea3667cb5a8
Luis Enrique <luiskike2210@gmail.com>
2023-09-11 15:58:39 -0600

    06 Metodos y Propiedades Strings

---
+++ strings2.js
@@ -0,0 +1,9 @@
+const text = 'EsTe eS uN tExTo PeRrOn';
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.charAt(2));
+console.log(text.indexOf('T'));
+console.log(text.startsWith('T'));
+console.log(text.endsWith('N'));
+console.log('*'.repeat(10));
