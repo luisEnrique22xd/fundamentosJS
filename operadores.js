f30b8105325e0208865672e7ed7409c2be787ec7
Luis Enrique <luiskike2210@gmail.com>
2023-09-11 15:46:29 -0600

    05 Operadores

---
+++ operadores.js
@@ -0,0 +1,33 @@
+//operadore Matematicos 
+// + - * / % **
+// += -= *= /= %= **=
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 * 10);
+console.log(10 / 10);
+console.log(10 % 10); //Modulo
+console.log(10 ** 10); //Potencia
+
+let num = 15;
+console.log(num += 5);
+console.log(num -= 5);
+console.log(num *= 5);
+console.log(num /= 5);
+console.log(num %= 5);
+
+//operadores booleanos
+// > < >= <= !== ===
+console.log(1>2);
+console.log(1<2);
+console.log(1>=2);
+console.log(1<=2);
+console.log(1!==2);
+console.log(1===2);
+
+//operadores logicos 
+// and &&
+console.log(true && true);
+// or || 
+console.log(true || false);
+// not ! 
+console.log(!true);
