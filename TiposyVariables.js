a163f9b15d1d6213c24e4f4d2362ac970f841749
Luis Enrique <luiskike2210@gmail.com>
2023-09-11 15:08:36 -0600

    02 Tipos de Variable y Tipos de dato

---
+++ TiposyVariables.js
@@ -0,0 +1,17 @@
+//Tipos en Javascript
+// number, string, boolean, object, undefined
+// typeof nos dice el tipo de dato
+console.log(12);
+console.log(typeof(12));
+
+ console.log(('string'));
+ console.log(typeof('string'));
+
+ console.log((true));
+ console.log(typeof(true));
+
+ let variable;
+ console.log(typeof(variable));
+
+ console.log([1,2,3]);
+ console.log(typeof([1,2,3]));
