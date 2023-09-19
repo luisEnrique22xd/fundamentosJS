293767a3e045da055403ff6f0a58af412f27f30c
Luis Enrique <luiskike2210@gmail.com>
2023-09-19 10:10:33 -0600

    11 ciclos

---
+++ reto1.js
@@ -0,0 +1,13 @@
+function wrapping(gifts) {
+    const envueltos = [];
+    for(const gift of gifts){
+      const tapaSuperior = '*'.repeat(gift.length + 2);
+      const tapaInferior = '*'.repeat(gift.length + 2);
+      envueltos.push(`${tapaSuperior}\n*${ gift }*\n${tapaInferior}`);
+    }
+    return envueltos;
+  }
+
+const gifts = ['cat', 'game', 'socks']
+const wrapped = wrapping(gifts)
+console.log(wrapped[0])
